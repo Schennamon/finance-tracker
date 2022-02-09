c1ef4aafee678d2a0a1db1d5927a9c8e35d1f159
schennamon <mr.tolokneev@gmail.com>
2022-02-09 20:47:49 +0200

    Display stock objects data in browser

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'stocks/search'
   get 'users/my_portfolio'
   devise_for :users
   root 'pages#index'
   get 'my_portfolio'=>'users#my_portfolio'
+  get 'stock_search'=>'stocks#search'
 end
